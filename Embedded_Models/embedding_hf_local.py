13fb4e4d3a97e30683c9285ed93d9ca70f5ab506
MohidNaghman1 <mohidnaghman315@gmail.com>
2025-05-29 06:30:02 -0700

    commit

--- Embedded_Models/embedding_hf_local.py
+++
@@ -1,8 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-text = "Islamabad is the capital of Pakistan"
-
-result = embeddings.embed_query(text)
-print(result)
